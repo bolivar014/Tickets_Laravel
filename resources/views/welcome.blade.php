a18a7319eaa43ae154c541397c0ed0fb2a100d42
Desarrollador Uno <desarrolladorunokubico@gmail.com>
2020-11-29 18:27:46 -0500

    Commit de prueba - Creación de Rutas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sistema de Tickets
                 </div>
 
                 <div class="links">
